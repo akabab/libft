f186792ada5eefe72a680a9f8cbc59d1f3187310
ycribier <ycribier@student.42.danstonbool.eu>
2015-01-29 19:55:37 +0100

    perror added

--- ft_perror.c
+++ ft_perror.c
@@ -6,12 +6,25 @@
 /*   By: ycribier <ycribier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/12/31 15:58:53 by ycribier          #+#    #+#             */
-/*   Updated: 2014/03/13 14:02:45 by ycribier         ###   ########.fr       */
+/*   Updated: 2015/01/29 19:51:10 by ycribier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include "libft.h"
+
+void	ft_perror(const char *msg)
+{
+	extern const int			errno;
+	extern const char * const	sys_errlist[];
+
+	if (msg)
+	{
+		ft_putstr_fd(msg, 2);
+		ft_putstr_fd(": ", 2);
+	}
+	ft_putendl_fd(sys_errlist[errno], 2);
+}
 
 void	error_msg(char *message)
 {
